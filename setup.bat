36ce105ccc62a8e304fe0c8c221cde503b8b6182
Raja Khan <rkhan@raileurope.com>
2017-04-11 14:56:49 -0400

    fixes

--- setup.bat
+++ setup.bat
@@ -4,4 +4,7 @@ docker build -t getrk/angular-client .
 
 docker push getrk/angular-client
 
+echo "Kill existing Container-ID if any:" 
+docker ps -aqf "name=angular-client"
+
 docker run -d --name angular-client -p 4200:4200 getrk/angular-client
